f4ff1636545684036be5499228fe42b16c153658
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2026-03-19 17:19:47 +0100

    feat: basic update on frontend to load image in search

--- SEAT_Models/SEAT_Angular_Page/src/app/components/seat-model/seat-model.ts
+++ SEAT_Models/SEAT_Angular_Page/src/app/components/seat-model/seat-model.ts
@@ -3,9 +3,9 @@ import { RouterLink } from '@angular/router';
 import { SeatModel } from '../../interfaces/seatModel';
 
 @Component({
-    selector: 'app-seat-model',
-    imports: [RouterLink],
-    template: `
+  selector: 'app-seat-model',
+  imports: [RouterLink],
+  template: `
     <section class="model-card">
       <h2 class="model-title">{{ seatModel().title }}</h2>
       <p class="model-url">
@@ -13,12 +13,15 @@ import { SeatModel } from '../../interfaces/seatModel';
           Wikipedia ↗
         </a>
       </p>
+      <p class="model-image">
+        <img [src]="seatModel().image" alt="{{ seatModel().title }}" />
+      </p>
       <a class="details-link" [routerLink]="['/details', modelIndex()]">Ver detalles</a>
     </section>
   `,
-    styleUrls: ['./seat-model.css'],
+  styleUrls: ['./seat-model.css'],
 })
 export class SeatModelComponent {
-    seatModel = input.required<SeatModel>();
-    modelIndex = input.required<number>();
+  seatModel = input.required<SeatModel>();
+  modelIndex = input.required<number>();
 }
--- SEAT_Models/SEAT_Angular_Page/src/app/interfaces/seatModel.ts
+++ SEAT_Models/SEAT_Angular_Page/src/app/interfaces/seatModel.ts
@@ -1,4 +1,5 @@
 export interface SeatModel {
     title: string;
     url: string;
+    image: string;
 }
